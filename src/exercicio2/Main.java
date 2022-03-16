96f500edf006433191db55002ca403378974abb4
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-03-15 22:31:38 -0300

    Exercício 2 refatorado

--- src/exercicio2/Main.java
+++ src/exercicio2/Main.java
@@ -7,22 +7,27 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int cont = 0;
+
         System.out.println("Escreva um número entre 350 e 8750: ");
         int numero = sc.nextInt();
-        if(numero < 350 && numero > 8750) {
-            System.out.println("Deve conter mais de 2 digitos");
+//        if(numero < 350 && numero > 8750) {
+//            System.out.println("Deve conter mais de 2 digitos");
+//            System.out.println("Escreva um número entre 350 e 8750: ");
+//            numero = sc.nextInt();
+
+        while(numero < 350 || numero > 8750) {
+            System.out.println("Deve conter mais de 2 digitos e no máximo 4 digitos entre 350 e 8750");
             System.out.println("Escreva um número entre 350 e 8750: ");
             numero = sc.nextInt();
         }
         String numStr = Integer.toString(numero); // Converte o mesmo em uma String
         char numArr[] = numStr.toCharArray();
-        for(int i = 0; i < numArr.length; i++)
-            if(numArr[i] % 2 == 1) {
+        for (int i = 0; i < numArr.length; i++)
+            if (numArr[i] % 2 == 1) {
                 cont++;
             }
 
         System.out.println("O número de ímpares é: " + cont);
     }
 }
-
 
